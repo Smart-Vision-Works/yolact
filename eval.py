3ce4eebc8e96af5b0f1a777e4cdae446c43d42bc
amteuscher <adamteuscher@smartvisionworks.com>
2022-07-19 14:16:18 -0600

    small changes to work with pipeline

--- eval.py
+++ eval.py
@@ -37,96 +37,6 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
-def parse_args(argv=None):
-    parser = argparse.ArgumentParser(
-        description='YOLACT COCO Evaluation')
-    parser.add_argument('--trained_model',
-                        default='weights/ssd300_mAP_77.43_v2.pth', type=str,
-                        help='Trained state_dict file path to open. If "interrupt", this will open the interrupt file.')
-    parser.add_argument('--top_k', default=5, type=int,
-                        help='Further restrict the number of predictions to parse')
-    parser.add_argument('--cuda', default=True, type=str2bool,
-                        help='Use cuda to evaulate model')
-    parser.add_argument('--fast_nms', default=True, type=str2bool,
-                        help='Whether to use a faster, but not entirely correct version of NMS.')
-    parser.add_argument('--cross_class_nms', default=False, type=str2bool,
-                        help='Whether compute NMS cross-class or per-class.')
-    parser.add_argument('--display_masks', default=True, type=str2bool,
-                        help='Whether or not to display masks over bounding boxes')
-    parser.add_argument('--display_bboxes', default=True, type=str2bool,
-                        help='Whether or not to display bboxes around masks')
-    parser.add_argument('--display_text', default=True, type=str2bool,
-                        help='Whether or not to display text (class [score])')
-    parser.add_argument('--display_scores', default=True, type=str2bool,
-                        help='Whether or not to display scores in addition to classes')
-    parser.add_argument('--display', dest='display', action='store_true',
-                        help='Display qualitative results instead of quantitative ones.')
-    parser.add_argument('--shuffle', dest='shuffle', action='store_true',
-                        help='Shuffles the images when displaying them. Doesn\'t have much of an effect when display is off though.')
-    parser.add_argument('--ap_data_file', default='results/ap_data.pkl', type=str,
-                        help='In quantitative mode, the file to save detections before calculating mAP.')
-    parser.add_argument('--resume', dest='resume', action='store_true',
-                        help='If display not set, this resumes mAP calculations from the ap_data_file.')
-    parser.add_argument('--max_images', default=-1, type=int,
-                        help='The maximum number of images from the dataset to consider. Use -1 for all.')
-    parser.add_argument('--output_coco_json', dest='output_coco_json', action='store_true',
-                        help='If display is not set, instead of processing IoU values, this just dumps detections into the coco json file.')
-    parser.add_argument('--bbox_det_file', default='results/bbox_detections.json', type=str,
-                        help='The output file for coco bbox results if --coco_results is set.')
-    parser.add_argument('--mask_det_file', default='results/mask_detections.json', type=str,
-                        help='The output file for coco mask results if --coco_results is set.')
-    parser.add_argument('--config', default=None,
-                        help='The config object to use.')
-    parser.add_argument('--output_web_json', dest='output_web_json', action='store_true',
-                        help='If display is not set, instead of processing IoU values, this dumps detections for usage with the detections viewer web thingy.')
-    parser.add_argument('--web_det_path', default='web/dets/', type=str,
-                        help='If output_web_json is set, this is the path to dump detections into.')
-    parser.add_argument('--no_bar', dest='no_bar', action='store_true',
-                        help='Do not output the status bar. This is useful for when piping to a file.')
-    parser.add_argument('--display_lincomb', default=False, type=str2bool,
-                        help='If the config uses lincomb masks, output a visualization of how those masks are created.')
-    parser.add_argument('--benchmark', default=False, dest='benchmark', action='store_true',
-                        help='Equivalent to running display mode but without displaying an image.')
-    parser.add_argument('--no_sort', default=False, dest='no_sort', action='store_true',
-                        help='Do not sort images by hashed image ID.')
-    parser.add_argument('--seed', default=None, type=int,
-                        help='The seed to pass into random.seed. Note: this is only really for the shuffle and does not (I think) affect cuda stuff.')
-    parser.add_argument('--mask_proto_debug', default=False, dest='mask_proto_debug', action='store_true',
-                        help='Outputs stuff for scripts/compute_mask.py.')
-    parser.add_argument('--no_crop', default=False, dest='crop', action='store_false',
-                        help='Do not crop output masks with the predicted bounding box.')
-    parser.add_argument('--image', default=None, type=str,
-                        help='A path to an image to use for display.')
-    parser.add_argument('--images', default=None, type=str,
-                        help='An input folder of images and output folder to save detected images. Should be in the format input->output.')
-    parser.add_argument('--video', default=None, type=str,
-                        help='A path to a video to evaluate on. Passing in a number will use that index webcam.')
-    parser.add_argument('--video_multiframe', default=1, type=int,
-                        help='The number of frames to evaluate in parallel to make videos play at higher fps.')
-    parser.add_argument('--score_threshold', default=0, type=float,
-                        help='Detections with a score under this threshold will not be considered. This currently only works in display mode.')
-    parser.add_argument('--dataset', default=None, type=str,
-                        help='If specified, override the dataset specified in the config with this one (example: coco2017_dataset).')
-    parser.add_argument('--detect', default=False, dest='detect', action='store_true',
-                        help='Don\'t evauluate the mask branch at all and only do object detection. This only works for --display and --benchmark.')
-    parser.add_argument('--display_fps', default=False, dest='display_fps', action='store_true',
-                        help='When displaying / saving video, draw the FPS on the frame')
-    parser.add_argument('--emulate_playback', default=False, dest='emulate_playback', action='store_true',
-                        help='When saving a video, emulate the framerate that you\'d get running in real-time mode.')
-
-    parser.set_defaults(no_bar=False, display=False, resume=False, output_coco_json=False, output_web_json=False, shuffle=False,
-                        benchmark=False, no_sort=False, no_hash=False, mask_proto_debug=False, crop=True, detect=False, display_fps=False,
-                        emulate_playback=False)
-
-    global args
-    args = parser.parse_args(argv)
-
-    if args.output_web_json:
-        args.output_coco_json = True
-    
-    if args.seed is not None:
-        random.seed(args.seed)
-
 iou_thresholds = [x / 100 for x in range(50, 100, 5)]
 coco_cats = {} # Call prep_coco_cats to fill this
 coco_cats_inv = {}
@@ -1046,11 +956,13 @@ def print_maps(all_maps):
 
 
 
-if __name__ == '__main__':
-    parse_args()
+def main(_args):
+    global args
+    global cfg
+    args = _args
 
     if args.config is not None:
-        set_cfg(args.config)
+        set_cfg(args.config, _dataset_path = args.dataset_path)
 
     if args.trained_model == 'interrupt':
         args.trained_model = SavePath.get_interrupt('weights/')
@@ -1103,3 +1015,95 @@ if __name__ == '__main__':
         if args.cuda:
             net = net.cuda()
         evaluate(net, dataset)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='YOLACT COCO Evaluation')
+    parser.add_argument('--trained_model',
+                        default='weights/ssd300_mAP_77.43_v2.pth', type=str,
+                        help='Trained state_dict file path to open. If "interrupt", this will open the interrupt file.')
+    parser.add_argument('--top_k', default=5, type=int,
+                        help='Further restrict the number of predictions to parse')
+    parser.add_argument('--cuda', default=True, type=str2bool,
+                        help='Use cuda to evaulate model')
+    parser.add_argument('--fast_nms', default=True, type=str2bool,
+                        help='Whether to use a faster, but not entirely correct version of NMS.')
+    parser.add_argument('--cross_class_nms', default=False, type=str2bool,
+                        help='Whether compute NMS cross-class or per-class.')
+    parser.add_argument('--display_masks', default=True, type=str2bool,
+                        help='Whether or not to display masks over bounding boxes')
+    parser.add_argument('--display_bboxes', default=True, type=str2bool,
+                        help='Whether or not to display bboxes around masks')
+    parser.add_argument('--display_text', default=True, type=str2bool,
+                        help='Whether or not to display text (class [score])')
+    parser.add_argument('--display_scores', default=True, type=str2bool,
+                        help='Whether or not to display scores in addition to classes')
+    parser.add_argument('--display', dest='display', action='store_true',
+                        help='Display qualitative results instead of quantitative ones.')
+    parser.add_argument('--shuffle', dest='shuffle', action='store_true',
+                        help='Shuffles the images when displaying them. Doesn\'t have much of an effect when display is off though.')
+    parser.add_argument('--ap_data_file', default='results/ap_data.pkl', type=str,
+                        help='In quantitative mode, the file to save detections before calculating mAP.')
+    parser.add_argument('--resume', dest='resume', action='store_true',
+                        help='If display not set, this resumes mAP calculations from the ap_data_file.')
+    parser.add_argument('--max_images', default=-1, type=int,
+                        help='The maximum number of images from the dataset to consider. Use -1 for all.')
+    parser.add_argument('--output_coco_json', dest='output_coco_json', action='store_true',
+                        help='If display is not set, instead of processing IoU values, this just dumps detections into the coco json file.')
+    parser.add_argument('--bbox_det_file', default='results/bbox_detections.json', type=str,
+                        help='The output file for coco bbox results if --coco_results is set.')
+    parser.add_argument('--mask_det_file', default='results/mask_detections.json', type=str,
+                        help='The output file for coco mask results if --coco_results is set.')
+    parser.add_argument('--config', default=None,
+                        help='The config object to use.')
+    parser.add_argument('--output_web_json', dest='output_web_json', action='store_true',
+                        help='If display is not set, instead of processing IoU values, this dumps detections for usage with the detections viewer web thingy.')
+    parser.add_argument('--web_det_path', default='web/dets/', type=str,
+                        help='If output_web_json is set, this is the path to dump detections into.')
+    parser.add_argument('--no_bar', dest='no_bar', action='store_true',
+                        help='Do not output the status bar. This is useful for when piping to a file.')
+    parser.add_argument('--display_lincomb', default=False, type=str2bool,
+                        help='If the config uses lincomb masks, output a visualization of how those masks are created.')
+    parser.add_argument('--benchmark', default=False, dest='benchmark', action='store_true',
+                        help='Equivalent to running display mode but without displaying an image.')
+    parser.add_argument('--no_sort', default=False, dest='no_sort', action='store_true',
+                        help='Do not sort images by hashed image ID.')
+    parser.add_argument('--seed', default=None, type=int,
+                        help='The seed to pass into random.seed. Note: this is only really for the shuffle and does not (I think) affect cuda stuff.')
+    parser.add_argument('--mask_proto_debug', default=False, dest='mask_proto_debug', action='store_true',
+                        help='Outputs stuff for scripts/compute_mask.py.')
+    parser.add_argument('--no_crop', default=False, dest='crop', action='store_false',
+                        help='Do not crop output masks with the predicted bounding box.')
+    parser.add_argument('--image', default=None, type=str,
+                        help='A path to an image to use for display.')
+    parser.add_argument('--images', default=None, type=str,
+                        help='An input folder of images and output folder to save detected images. Should be in the format input->output.')
+    parser.add_argument('--video', default=None, type=str,
+                        help='A path to a video to evaluate on. Passing in a number will use that index webcam.')
+    parser.add_argument('--video_multiframe', default=1, type=int,
+                        help='The number of frames to evaluate in parallel to make videos play at higher fps.')
+    parser.add_argument('--score_threshold', default=0, type=float,
+                        help='Detections with a score under this threshold will not be considered. This currently only works in display mode.')
+    parser.add_argument('--dataset', default=None, type=str,
+                        help='If specified, override the dataset specified in the config with this one (example: coco2017_dataset).')
+    parser.add_argument('--detect', default=False, dest='detect', action='store_true',
+                        help='Don\'t evauluate the mask branch at all and only do object detection. This only works for --display and --benchmark.')
+    parser.add_argument('--display_fps', default=False, dest='display_fps', action='store_true',
+                        help='When displaying / saving video, draw the FPS on the frame')
+    parser.add_argument('--emulate_playback', default=False, dest='emulate_playback', action='store_true',
+                        help='When saving a video, emulate the framerate that you\'d get running in real-time mode.')
+
+    parser.set_defaults(no_bar=False, display=False, resume=False, output_coco_json=False, output_web_json=False, shuffle=False,
+                        benchmark=False, no_sort=False, no_hash=False, mask_proto_debug=False, crop=True, detect=False, display_fps=False,
+                        emulate_playback=False)
+
+    global args
+    args = parser.parse_args()
+    setattr(args, "dataset_path", "/auto/shared/client_data/image_database/COCO_format_datasets/rollerInstanceSegmentation-2022-06-29-15-48-49.546778")
+    main(args)
+
+    if args.output_web_json:
+        args.output_coco_json = True
+    
+    if args.seed is not None:
+        random.seed(args.seed)
