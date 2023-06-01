ca8026903dd721a1ec4ae07ede7ea6661db08a8a
mheydorn <matthew@smartvisionworks.com>
2023-06-01 15:33:39 -0600

    remove --cuda

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python train.py  --config rollerInstanceSegmentation_dataset --dataset rollerInstanceSegmentation_dataset --cuda 1 --num_workers=4 --save_interval 10000
+python train.py  --config rollerInstanceSegmentation_dataset --dataset rollerInstanceSegmentation_dataset --num_workers=4 --save_interval 10000
